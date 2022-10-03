794eff4665e0fe704005d38ab2f018f9310581c2
sangdx <sangdx@vmodev.com>
2022-10-03 16:30:50 +0700

    test8

--- lib/presentation/view/movie_detail/movie_detail_screen.dart
+++ lib/presentation/view/movie_detail/movie_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:dio/dio.dart';
 import 'package:project_demo/presentation/common/skeleton/skeleton_movie_detail.dart';
 import 'package:project_demo/presentation/view/movie_detail/movie_detail_body.dart';
 import 'package:project_demo/presentation/view/movie_detail/movie_detail_cubit/movie_detail_cubit.dart';
